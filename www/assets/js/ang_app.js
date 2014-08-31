241da62a82cc0c4bb7224cc34fe518f235356ade
kevin <kgrabiner75@gmail.com>
2014-08-31 22:58:06 +0200

    git ajout de angular touch pour mobile

--- www/assets/js/ang_app.js
+++ www/assets/js/ang_app.js
@@ -1,5 +1,5 @@
 //Define an angular module for our app
-var app = angular.module('mobile', ['ui-rangeSlider']);
+var app = angular.module('mobile', ['ui-rangeSlider', 'ngTouch']);
 
 app.controller('mobileController', function($scope, $http) {
 
@@ -160,6 +160,12 @@ app.controller('mobileController', function($scope, $http) {
 
 
 
+    };
+
+    $scope.gotoiti = function(resultat) {
+        $scope.resultat = resultat;
+
+        $.mobile.changePage('#parcoursItin');
     };
 
 });
